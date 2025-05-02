bfd86369dc671aab7ef3cd88f7f64d18a429efe1
Eduualvarez <educhuy-2014@hotmail.com>
2025-05-01 23:47:56 -0300

    .

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-
+import { authRole } from "../middlewares/authRole.middleware.js";
 import {editProductSchema} from '../schemas/products.schema.js'
 import {productController} from "../controllers/product.controller.js"
 
@@ -10,10 +10,10 @@ router.get("/", productController.getAllProducts);
 
 router.get("/:pid",productController.getById);
 
-router.delete("/:pid",productController.deleteOne);
+router.delete("/:pid",authRole(["admin"]),productController.deleteOne);
 
-router.put("/", validateSchema(editProductSchema),productController.updateProduct);
+router.put("/", validateSchema(editProductSchema),authRole(["admin"]),productController.updateProduct);
 
-router.post("/",  productController.createProduct);
+router.post("/", authRole(["admin"]), productController.createProduct);
 
 export default router;
